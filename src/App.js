483fcc9711b415c235efea2eea0482d5f48dcc68
EglanyJr <eglanyjr@gmail.com>
2022-01-10 15:50:11 -0400

    feat: create router in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route patch="/" component={Login} />
+      <Route patch="/wallet" component={Wallet} />
+    </Switch>
+  );
 }
 
 export default App;
